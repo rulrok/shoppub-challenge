c7607f4a7bdcbf0e61023d6a5638bd4b20836439
Reuel R. Ribeiro <rulrok@gmail.com>
2020-01-12 23:57:47 -0300

    Correctly create product tag this time

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,24 +1,27 @@
 <template>
     <div>
         <product-images/>
-        
-        <CampaignTag v-for="tag in tags" :key="tag.label" :label="tag.label" :color="tag.color" />
-        
+
+        <CampaignTag v-for="tag in campaign_tags" :key="tag.label" :label="tag.label" :color="tag.color"/>
+
         <p>{{title}}</p>
-        
-        
+
+        <ProductTag v-for="tag in tags" :key="tag.label" :label="tag.label" :color="tag.color" />
     </div>
 </template>
 <script>
-import ProductImages from "./ProductImages";
-import CampaignTag from "./CampaignTag";
-export default {
-    props: {
-      id: Number,
-      title: String,
-      images: Array,
-      tags: Array  
-    },
-    components: {CampaignTag, ProductImages},
-};
+    import ProductImages from "./ProductImages";
+    import CampaignTag from "./CampaignTag";
+    import ProductTag from "./ProductTag";
+
+    export default {
+        props: {
+            id: Number,
+            title: String,
+            images: Array,
+            tags: Array,
+            campaign_tags: Array
+        },
+        components: {ProductTag, CampaignTag, ProductImages},
+    };
 </script>
---
+++ src/components/ProductTag.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="product-tag" :style="`background-color: ${color}; color: white;`">
+        {{label}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ProductTag",
+        props: {
+            label: {
+                type: String,
+                required: true,
+            },
+            color: {
+                type: String,
+                default: "#CCC",
+            },
+        },
+    };
+</script>
+
+<style scoped>
+    .product-tag {
+        padding: 7px;
+        max-width: 150px;
+    }
+</style>

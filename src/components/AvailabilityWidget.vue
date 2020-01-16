6e09bc87899258fdd2948f858778688a9a073303
Reuel R. Ribeiro <rulrok@gmail.com>
2020-01-16 16:23:02 -0300

    Improve AvailabilityWidget.vue responsiveness desktop/mobile

--- src/components/AvailabilityWidget.vue
+++ src/components/AvailabilityWidget.vue
@@ -1,17 +1,33 @@
 <template>
-    <div class="availability-widget columns is-vcentered">
-        <div class="column is-narrow">
+    <div class="availability-widget columns is-gapless">
+        <!-- non-mobile input left side -->
+        <div class="column is-narrow is-hidden-mobile">
             <input
                     class="quantity-input"
-                    type="text" step="1" min="1" :max="maxAvailabilityInitial"
+                    type="text" step="1" min="1" :max="maxAvailability"
                     v-model="selectedQuantity">
         </div>
-        <div class="column is-narrow" style="padding-left: 0; padding-right: 0;">
+
+        <!-- increase/decrease controls -->
+        <div class="column is-2-tablet is-full-mobile">
             <div class="quantity-control-container">
-                <button class="quantity-control" @click="increaseQuantity">+</button>
-                <button class="quantity-control" @click="decreaseQuantity">-</button>
+                <div class="columns is-gapless is-mobile is-multiline">
+                    <div class="column is-full-tablet is-3-mobile">
+                        <button class="quantity-control is-hidden-mobile" @click="increaseQuantity">+</button>
+                        <button class="quantity-control mobile-quantity-control is-hidden-tablet" @click="decreaseQuantity">-</button>
+                    </div>
+                    <div class="column is-hidden-tablet">
+                        <input class="mobile-quantity-input" type="text" step="1" min="1" :max="maxAvailability" v-model="selectedQuantity">
+                    </div>
+                    <div class="column is-full-tablet is-3-mobile">
+                        <button class="quantity-control is-hidden-mobile" @click="decreaseQuantity">-</button>
+                        <button class="quantity-control mobile-quantity-control is-hidden-tablet" @click="increaseQuantity">+</button>
+                    </div>
+                </div>
             </div>
         </div>
+
+        <!-- Add button right/bottom side -->
         <div class="column">
             <button class="add-quantity" @click="addQuantity">ADICIONAR</button>
         </div>
@@ -70,43 +86,76 @@
 <style scoped lang="scss">
     @import "src/styles/variables";
 
-    $basic-input-height: 4em;
+    @import "node_modules/bulma/sass/utilities/_all.sass";
+    @import "node_modules/bulma/sass/base/_all.sass";
+    @import "node_modules/bulma/sass/utilities/mixins";
+
+    $spacing: .33em;
+    $basic-input-height: 3.3em + $spacing;
+    $mobile-input-height: 2em;
+
+    .availability-widget {
+        max-height: $basic-input-height;
+    }
+
+    .availability-widget .column.is-2-tablet > .quantity-control-container > .columns > .column.is-hidden-tablet {
+        /* Spacing for number input on mobile size */
+        padding: 0 $spacing !important;
+    }
 
     .quantity-input {
         width: $basic-input-height;
         height: $basic-input-height;
+    }
 
+    .mobile-quantity-input {
+        width: 100%;
+        height: $mobile-input-height;
+    }
+
+    .mobile-quantity-control {
+        height: $mobile-input-height !important;
+    }
+
+    .quantity-input, .mobile-quantity-input {
         border: 1px solid $border-color;
-
         text-align: center;
     }
 
-    $quantity-button-spacing: 0.3em;
     .quantity-control-container {
-        & > button.quantity-control {
+
+        @include tablet() {
+            margin-left: $spacing;
+            margin-right: $spacing;
+        }
+
+        @include mobile() {
+            margin-bottom: $spacing;
+        }
+
+        button.quantity-control {
 
             background-color: $base-color-light;
             border: none;
             color: white;
 
-            height: calc((#{$basic-input-height} - 2 * #{$quantity-button-spacing}) / 2);
-            width: 2.5em;
+            height: calc((#{$basic-input-height} - 2 * #{$spacing}) / 2);
+            width: 100%;
             font-size: 1em;
+            text-align: center;
 
-            &:first-child {
-                display: block;
-                margin-bottom: $quantity-button-spacing;
-            }
         }
     }
 
     button.add-quantity {
         width: 100%;
-        height: 3.3em;
-
+        height: $basic-input-height;
+        @include mobile() {
+            height: $mobile-input-height * 1.5;
+        }
         background-color: $primary-color;
         border: none;
         color: white;
-        font-size: 1.15em;
+        text-align: center;
     }
 </style>
